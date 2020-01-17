d1a4f5c7314f2a8a1968ef8d09a8f19df76d6250
Mateusz Lazar <mateuszlazar@outlook.com>
2020-01-17 22:35:40 +0100

    feat: first stage of moving static sites from old website

---
+++ src/components/Hero.js
@@ -0,0 +1,10 @@
+import React from "react";
+import fullLogo from "../img/logo-full.png";
+
+const Hero = () => (
+  <div style={{ backgroundColor: "#f9f9f9" }}>
+    <img className="full-width-image-fit" src={fullLogo} />
+  </div>
+);
+
+export default Hero;
--- src/pages/products/index.md
+++ src/pages/products/index.md
@@ -1,5 +1,5 @@
 ---
-templateKey: 'product-page'
+templateKey: 'hero-page'
 path: /products
 title: Our Coffee
 image: /img/jumbotron.jpg
