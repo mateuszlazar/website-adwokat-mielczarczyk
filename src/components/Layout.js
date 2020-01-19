497e8e8c277dead43bca4edff7ec35c36db0e186
Mateusz Lazar <mateuszlazar@outlook.com>
2020-01-19 21:26:10 +0100

    feat: cookies

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -7,6 +7,7 @@ import "./all.sass";
 import useSiteMetadata from "./SiteMetadata";
 import { withPrefix } from "gatsby";
 import { Favicon } from "./Favicon";
+import CookieConsent from "react-cookie-consent";
 
 const TemplateWrapper = ({ children, pageTitle = null, showBrand = true }) => {
   const { title, description } = useSiteMetadata();
@@ -29,6 +30,20 @@ const TemplateWrapper = ({ children, pageTitle = null, showBrand = true }) => {
       {showBrand && <Hero />}
       <div>{children}</div>
       <Footer />
+      <CookieConsent
+        location="bottom"
+        buttonText="Zgadzam się"
+        style={{ background: "#CC3700" }}
+        buttonStyle={{
+          color: "#000",
+          fontSize: "14px",
+          backgroundColor: "#fff"
+        }}
+      >
+        Ta strona wykorzystuje pliki cookie. Jeśli nie wyrażasz zgody,
+        ustawienia dotyczące plików cookies możesz zmienić w swojej
+        przeglądarce.
+      </CookieConsent>
     </div>
   );
 };
